e80e65d07b8a0ce19a51238a5c4ca10af2ee1e29
bharadwajks <54477571+bharadwajks@users.noreply.github.com>
2022-12-28 22:24:40 +0530

    Update akshay.py

--- akshay.py
+++ akshay.py
@@ -10,5 +10,11 @@ def getKey(obj: dict):
     keys = list(obj)
     if len(keys) != 1:
         raise Exception('either multiple keys or empty dict found')
-    else:
+    else
         return keys[0]
+    def getNestedValue(obj: dict, key: str, isFound = False):
+    # print(obj, key, isFound)
+    if type(obj) is not dict and not isFound:
+        return None
+    if (isFound or (key in obj.keys())) 
+        if type(obj[key]) is dict:
