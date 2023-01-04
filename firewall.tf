2d7c4582fd56df4743d1efc27fe5aee15107b3d5
bharadwajks <54477571+bharadwajks@users.noreply.github.com>
2023-01-04 12:11:00 +0530

    Update firewall.tf

--- firewall.tf
+++ firewall.tf
@@ -13,3 +13,6 @@ resource "google_compute_firewall" "allow-traffic" {
   source_tags   = ["akshayapp_server-firewall"]
   source_ranges = ["0.0.0.0/0"]
 }
+
+
+# Note here that in thr line number 5 I am allowing icmp so that , allows ingress, or inbound, ICMP traffic from any source to any instance on the network.
